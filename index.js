01aba687eff9641911ed693ef367ede8f8a63bae
Vyomesh Saini <121440682+INDTMExpert@users.noreply.github.com>
2026-04-30 12:22:51 +0530

    added input fields and buttons
    added some input fields and buttons in consent form

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById("consent-form")
  
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -8,4 +9,9 @@ setTimeout(function(){
 
 modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
+})
+
+consentForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    
 })
