869be12e91c27fffebfb5c6655cd89fbc020f6c6
Vyomesh Saini <121440682+INDTMExpert@users.noreply.github.com>
2026-04-30 12:35:01 +0530

    Added after submit decoration

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById("consent-form")
- 
+const modalText = document.getElementById('modal-text')
 setTimeout(function(){
     modal.style.display = 'inline'
 }, 1500)
@@ -13,5 +13,10 @@ modalCloseBtn.addEventListener('click', function(){
 
 consentForm.addEventListener('submit',function(e){
     e.preventDefault();
-    
+    modalText.innerHTML = `<div class="modal-inner-loading">
+                            <img src="images/loading.svg" class="loading">
+                            <p id="uploadText">
+                                Uploading your data to the dark web...
+                            </p>
+                        </div>`
 })
