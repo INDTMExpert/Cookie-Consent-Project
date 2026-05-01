2a69cf2fe4bb688c1cb588ec41e7b255b9b74838
Vyomesh Saini <121440682+INDTMExpert@users.noreply.github.com>
2026-05-01 16:34:41 +0530

    small modification in cookie consent page

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
+const modal = document.getElementById("modal");
+const modalCloseBtn = document.getElementById("modal-close-btn");
+const consentForm = document.getElementById("consent-form");
+const modalText = document.getElementById("modal-text");
+setTimeout(function () {
+  modal.style.display = "inline";
+}, 1500);
 
-const modal = document.getElementById('modal')
-const modalCloseBtn = document.getElementById('modal-close-btn')
-const consentForm = document.getElementById("consent-form")
-const modalText = document.getElementById('modal-text')
-setTimeout(function(){
-    modal.style.display = 'inline'
-}, 1500)
+modalCloseBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+});
 
-modalCloseBtn.addEventListener('click', function(){
-    modal.style.display = 'none'
-})
+consentForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const formData = new FormData(consentForm);
+//   name from formData Object
+  const name = formData.get('fullName'); //fullName is object of name used in input tag in HTML
 
-consentForm.addEventListener('submit',function(e){
-    e.preventDefault();
-    modalText.innerHTML = `<div class="modal-inner-loading">
+
+  modalText.innerHTML = `<div class="modal-inner-loading">
                             <img src="images/loading.svg" class="loading">
                             <p id="uploadText">
                                 Uploading your data to the dark web...
                             </p>
-                        </div>`
-})
+                        </div>`;
+
+  setTimeout(() => {
+    document.getElementById("uploadText").textContent = `Making the sale...`;
+  }, 1500);
+
+  setTimeout(function () {
+    document.getElementById("modal-inner").innerHTML =
+                                        `  <h2>Thanks <span class="modal-display-name">${name}</span>, you sucker! </h2>
+                                            <p>We just sold the rights to your eternal soul.</p>
+                                                <div class="idiot-gif">
+                                                    <img src="images/pirate.webp">
+                                                </div> `;
+  }, 3000);
+});
